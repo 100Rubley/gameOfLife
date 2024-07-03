e61348f33bf9c53336316c5f9fc00a9fca4009d1
Антон Казаков <kazakov_dev@MacBook-Air-Anton.local>
2024-07-03 12:37:45 +0300

    add render method

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let grid, cols = 10, rows = 10;
+// DOM objects start
+const canvas = document.querySelector('.canvas');
+// DOM objects end
+
+const cellSize = 10;
+canvas.width = 600;
+canvas.height = 400;
+const ctx = canvas.getContext('2d');
+let grid, cols, rows;
 
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
@@ -21,8 +29,25 @@ function fillWithRandom(grid) {
 }
 
 function setup() {
+  cols = canvas.width / cellSize;
+  rows = canvas.height / cellSize;
   grid = fillWithRandom(make2DArray(cols, rows))
 }
 
+function render() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j] === 1) {
+        let x = i * cellSize;
+        let y = j * cellSize;
+
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, y, cellSize - 1, cellSize - 1);
+      }
+    }
+  }
+}
+
 setup()
+render()
 console.table(grid)
